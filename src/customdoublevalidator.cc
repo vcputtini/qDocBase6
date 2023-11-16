a0c916649f2471c57f8f51825124f673dddc84f4
vcputtini <vcputtini@gmail.com>
2023-11-16 17:22:22 -0300

    Code adjustments to general standards used in coding.

--- src/customdoublevalidator.cc
+++ src/customdoublevalidator.cc
@@ -1,54 +1,99 @@
+/***************************************************************************
+ * Copyright (c) 2007-2023                                                 *
+ *      Volnei Cervi Puttini.  All rights reserved.                        *
+ *      vcputtini@gmail.com
+ *                                                                         *
+ * Redistribution and use in source and binary forms, with or without      *
+ * modification, are permitted provided that the following conditions      *
+ * are met:                                                                *
+ * 1. Redistributions of source code must retain the above copyright       *
+ *    notice, this list of conditions and the following disclaimer.        *
+ * 2. Redistributions in binary form must reproduce the above copyright    *
+ *    notice, this list of conditions and the following disclaimer in the  *
+ *    documentation and/or other materials provided with the distribution. *
+ * 4. Neither the name of the Author     nor the names of its contributors *
+ *    may be used to endorse or promote products derived from this software*
+ *    without specific prior written permission.                           *
+ *                                                                         *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   *
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR      *
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS  *
+ * BE LIABLEFOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   *
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS*
+ * INTERRUPTION)                                                           *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,     *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING   *
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      *
+ * POSSIBILITY OFSUCH DAMAGE.                                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include "customdoublevalidator.h"
 
-CustomDoubleValidator::CustomDoubleValidator(double bottom,
-                                             double top,
-                                             int decimals,
-                                             QObject* parent)
-  : QDoubleValidator(bottom, top, decimals, parent)
+#include <climits>
+
+CustomDoubleValidator::CustomDoubleValidator(double bottom_,
+                                             double top_,
+                                             int decimals_,
+                                             QObject* parent_)
+  : QDoubleValidator(bottom_, top_, decimals_, parent_)
 {
 }
 
 QValidator::State
-CustomDoubleValidator::validate(QString& input, int& pos) const
+CustomDoubleValidator::validate(QString& input_, int& pos_) const
 {
-  QString buff(input);
+  QString buff(input_);
 
-  if (buff.isEmpty())
+  if (buff.isEmpty()) {
     return QValidator::Acceptable;
+  }
 
-  double t;
-  double b;
-  double d;
+  double t_{ 0.0 };
+  double b_{ 0.0 };
+  double d_{ 0.0 };
 
   QByteArray abuff;
-  if (!validateChars(input, DoubleStandardMode, &abuff, decimals())) {
+  if (!validateChars(input_, DoubleStandardMode, &abuff, decimals())) {
     return Invalid;
   }
 
-  t = buff.section('.', 0, 0).length();
-  d = buff.section('.', 1, 2).length();
+  // t = buff.section('.', 0, 0).length();
+  d_ = buff.section('.', 1, 2).length();
 
-  if (bottom() >= 0 && buff.startsWith('-'))
+  if (bottom() >= 0 && buff.startsWith('-')) {
     return Invalid;
+  }
 
-  if (top() < 0 && buff.startsWith('+'))
+  if (top() < 0 && buff.startsWith('+')) {
     return Invalid;
+  }
 
-  if (d > decimals())
+  if (d_ > decimals()) {
     return QValidator::Invalid;
+  }
 
-  if (buff.toDouble() < bottom() || buff.toDouble() > top())
+  if (buff.toDouble() < bottom() || buff.toDouble() > top()) {
     return QValidator::Invalid;
+  }
 
-  /* Testa os limites do numero
-     ex: se maximo e 999999999.99 permite digitar ate 999999999.00
-  */
+  /*
+   * Test the limits of number
+   * ex: if the maximum is 999999999.99, it allows you to type up to
+   *  999999999.00
+   */
   double i = buff.toDouble();
-  b = bottom();
-  t = top();
+  b_ = bottom();
+  t_ = top();
 
   if (notation() == StandardNotation) {
-    double max = qMax(qAbs(b), qAbs(t));
+    double max = qMax(qAbs(b_), qAbs(t_));
     if (max < LLONG_MAX) {
       double n = qPow(numDigits(qlonglong(max)), 10);
       if (qAbs(i) > n)
@@ -75,43 +120,44 @@ CustomDoubleValidator::numDigits(qlonglong n) const
  *        O codigo foi alterado para minhas necessidades particularidades
  */
 bool
-CustomDoubleValidator::validateChars(const QString& str,
-                                     NumberMode numMode,
-                                     QByteArray* buff,
-                                     int decDigits) const
+CustomDoubleValidator::validateChars(const QString& str_,
+                                     NumberMode numMode_,
+                                     QByteArray* buff_,
+                                     int decDigits_) const
 {
-  buff->clear();
-  buff->reserve(str.length());
+  buff_->clear();
+  buff_->reserve(str_.length());
 
-  const bool scientific = numMode == DoubleScientificMode;
-  bool lastWasE = false;
-  bool lastWasDigit = false;
-  int decPointCnt = 0;
-  bool dec = false;
-  int decDigitCnt = 0;
+  const bool scientific_ = numMode_ == DoubleScientificMode;
+  bool lastWasE_{ false };
+  bool lastWasDigit_{ false };
+  int decPointCnt_{ 0 };
+  bool dec_{ false };
+  int decDigitCnt_{ 0 };
 
-  for (int i = 0; i < str.length(); ++i) {
-    char c = digitToCLocale(str.at(i));
+  for (int i = 0; i < str_.length(); ++i) {
+    char c_ = digitToCLocale(str_.at(i));
 
-    if (c >= '0' && c <= '9') {
-      if (numMode != IntegerMode) {
+    if (c_ >= '0' && c_ <= '9') {
+      if (numMode_ != IntegerMode) {
         // If a double has too many digits after decpt, it shall be Invalid.
-        if (dec && decDigits != -1 && decDigits < ++decDigitCnt)
+        if (dec_ && decDigits_ != -1 && decDigits_ < ++decDigitCnt_)
           return false;
       }
-      lastWasDigit = true;
+      lastWasDigit_ = true;
     } else {
-      switch (c) {
+      switch (c_) {
         case ',': // nao faz nada. A virgula DEVE ter o mesmo comportamento do
                   // ponto decimal
         case '.':
-          if (numMode == IntegerMode) {
+          if (numMode_ == IntegerMode) {
             // If an integer has a decimal point, it shall be Invalid.
             return false;
           } else {
             // If a double has more than one decimal point, it shall be Invalid.
-            if (++decPointCnt > 1)
+            if (++decPointCnt_ > 1) {
               return false;
+            }
 #if 0
                         // If a double with no decimal digits has a decimal point, it shall be
                         // Invalid.
@@ -119,77 +165,86 @@ CustomDoubleValidator::validateChars(const QString& str,
                             return false;
 #endif // On second thoughts, it shall be Valid.
 
-            dec = true;
+            dec_ = true;
           }
           break;
 
         case '+':
         case '-':
-          if (scientific) {
+          if (&scientific) {
             // If a scientific has a sign that's not at the beginning or after
             // an 'e', it shall be Invalid.
-            if (i != 0 && !lastWasE)
+            if (i != 0 && !lastWasE_) {
               return false;
+            }
           } else {
             // If a non-scientific has a sign that's not at the beginning,
             // it shall be Invalid.
-            if (i != 0)
+            if (i != 0) {
               return false;
+            }
           }
           break;
         default:
           // If it's not a valid digit, it shall be Invalid.
           return false;
       }
-      lastWasDigit = false;
+      lastWasDigit_ = false;
     }
 
-    lastWasE = c == 'e';
-    if (c != ',')
-      buff->append(c);
+    lastWasE_ = c_ == 'e';
+    if (c_ != ',') {
+      buff_->append(c_);
+    }
   }
 
   return true;
 }
 
-/**
- * @bried Adaptação do codigo original dos fontes da Nokia Qt Framework Qt 4.7.1
- *        a fim de suprir algumas necessidades especificas
- *        O codigo foi alterado para minhas necessidades particularidades
+/*!
+ * \brief Adaptation of the original source code from Nokia Qt Framework
+ * Qt 4.7.1 in order to meet some specific needs.
  */
 char
-CustomDoubleValidator::digitToCLocale(const QChar& in) const
+CustomDoubleValidator::digitToCLocale(const QChar& in_) const
 {
-  const QChar _zero = '0';
-  const QChar _group = ',';
-  const ushort zeroUnicode = _zero.unicode();
-  const ushort tenUnicode = zeroUnicode + 10;
+  const QChar zero_ = '0';
+  const QChar group_ = ',';
+  const ushort zeroUnicode_ = zero_.unicode();
+  const ushort tenUnicode_ = zeroUnicode_ + 10;
 
-  if (in.unicode() >= zeroUnicode && in.unicode() < tenUnicode)
-    return '0' + in.unicode() - zeroUnicode;
+  if (in_.unicode() >= zeroUnicode_ && in_.unicode() < tenUnicode_) {
+    return '0' + in_.unicode() - zeroUnicode_;
+  }
 
-  if (in.unicode() >= '0' && in.unicode() <= '9')
-    return in.toLatin1();
+  if (in_.unicode() >= '0' && in_.unicode() <= '9') {
+    return in_.toLatin1();
+  }
 
-  if (in == '+')
+  if (in_ == '+') {
     return '+';
+  }
 
-  if (in == '-')
+  if (in_ == '-') {
     return '-';
+  }
 
-  if (in == '.')
+  if (in_ == '.') {
     return '.';
+  }
 
-  if (in == ',')
+  if (in_ == ',') {
     return ',';
+  }
 
   // if (in == exponential() || in == exponential().toUpper())
   //     return 'e';
 
   // In several languages group() is the char 0xA0, which looks like a space.
   // People use a regular space instead of it and complain it doesn't work.
-  if (_group.unicode() == 0xA0 && in.unicode() == ' ')
+  if (group_.unicode() == 0xA0 && in_.unicode() == ' ') {
     return ',';
+  }
 
   return 0;
 }
@@ -198,10 +253,10 @@ CustomDoubleValidator::digitToCLocale(const QChar& in) const
  * @bried Funcao para calculo de uma potencia de 10
  */
 qlonglong
-CustomDoubleValidator::pow10(int exp) const
+CustomDoubleValidator::pow10(int n_) const
 {
   qlonglong result = 1;
-  for (int i = 0; i < exp; ++i)
+  for (int i = 0; i < n_; ++i)
     result *= 10;
   return result;
 }
--- src/customdoublevalidator.h
+++ src/customdoublevalidator.h
@@ -1,3 +1,39 @@
+/***************************************************************************
+ * Copyright (c) 2007-2023                                                 *
+ *      Volnei Cervi Puttini.  All rights reserved.                        *
+ *      vcputtini@gmail.com
+ *                                                                         *
+ * Redistribution and use in source and binary forms, with or without      *
+ * modification, are permitted provided that the following conditions      *
+ * are met:                                                                *
+ * 1. Redistributions of source code must retain the above copyright       *
+ *    notice, this list of conditions and the following disclaimer.        *
+ * 2. Redistributions in binary form must reproduce the above copyright    *
+ *    notice, this list of conditions and the following disclaimer in the  *
+ *    documentation and/or other materials provided with the distribution. *
+ * 4. Neither the name of the Author     nor the names of its contributors *
+ *    may be used to endorse or promote products derived from this software*
+ *    without specific prior written permission.                           *
+ *                                                                         *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   *
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR      *
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS  *
+ * BE LIABLEFOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   *
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS*
+ * INTERRUPTION)                                                           *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,     *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING   *
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      *
+ * POSSIBILITY OFSUCH DAMAGE.                                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifndef CUSTOMDOUBLEVALIDATOR_H
 #define CUSTOMDOUBLEVALIDATOR_H
 
@@ -7,8 +43,8 @@
 
 #include <QtMath>
 
-#include <limits.h>
-#include <math.h>
+#include <climits>
+#include <cmath>
 
 #ifndef LLONG_MAX
 #define LLONG_MAX Q_INT64_C(0x7fffffffffffffff)
@@ -17,54 +53,43 @@
 using namespace std;
 
 /*!
- * \brief Valida corretamente a digitacao de numero com ponto decimal
  * \class CustomDoubleValidator
- * \author Volnei Cervi Puttini
- * \date 2011-02-17
+ * \brief Valida corretamente a digitacao de numero com ponto decimal
  *
- * Faz com que os parametros top, bottol e decimals sejam rigorosamente
- * respeitados durante a digitacao do numero decimal.
- * Ou seja, se um numero for definido como por exemplo: bottom=0.0 top=9999.99
- * decimals=2 ira permitir a digitacao do numero apenas dentro dos limites
- * estabelecidos. no formato 9999.99 como dado neste exemplo.
- *
- * %ChangeLog%
- * 2013-05-23 Atualizacao para versao Qt5
+ * It ensures that the top, bottom and decimals parameters are strictly
+ * respected when typing the decimal number. In other words, if a number is
+ * defined as: bottom=0.0 top=9999.99 decimals=2 it will allow the number to be
+ * entered only within the established limits. in the format 9999.99 as given in
+ * this example.
  *
  */
-
 class CustomDoubleValidator : public QDoubleValidator
 {
   Q_OBJECT
 public:
-  CustomDoubleValidator(double bottom,
-                        double top,
-                        int decimals,
-                        QObject* parent = 0);
+  CustomDoubleValidator(double bottom_,
+                        double top_,
+                        int decimals_,
+                        QObject* parent_ = nullptr);
 
-  virtual QValidator::State validate(QString& input, int& pos) const;
+  virtual QValidator::State validate(QString& input_, int& pos_) const;
 
 private:
-  /* Funcoes auxiliares */
   enum NumberMode
   {
     IntegerMode,
     DoubleStandardMode,
     DoubleScientificMode
   };
-  bool validateChars(const QString& str,
-                     NumberMode numMode,
-                     QByteArray* buff,
-                     int decDigits) const;
+  bool validateChars(const QString& str_,
+                     NumberMode numMode_,
+                     QByteArray* buff_,
+                     int decDigits_) const;
 
-  inline int numDigits(qlonglong n) const;
-  inline char digitToCLocale(const QChar& in) const;
+  inline int numDigits(qlonglong n_) const;
+  inline char digitToCLocale(const QChar& in_) const;
 
-  qlonglong pow10(int n) const;
-
-signals:
-
-public slots:
+  qlonglong pow10(int n_) const;
 };
 
 #endif // CUSTOMDOUBLEVALIDATOR_H
